a22cde89898bfbe76b102e26cd410d16b46f9111
juangil991 <jcgo125@gmail.com>
2022-02-23 21:16:34 -0500

    Se hace conexion con base de datos y se reorganizan las clases en sus respectivos paquetes

---
+++ back/src/main/java/co/com/sofka/crud/Dto/TodoDto.java
@@ -0,0 +1,45 @@
+package co.com.sofka.crud.Dto;
+
+import java.io.Serializable;
+
+public class TodoDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private boolean completed;
+    private String groupListId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getGroupListId() {
+        return groupListId;
+    }
+
+    public void setGroupListId(String groupListId) {
+        this.groupListId = groupListId;
+    }
+}
